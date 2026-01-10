eb3cfd3ac6db985de327914a4d5737baa72ed45c
kossyak <akosyak@protonmail.com>
2026-01-10 21:54:13 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@ function add(a, b) {
     return a + b;
 }
 
-add(3, 4)
+add('hgkhg', 4)
+
---
+++ src/prototy.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+class Prototy {
+	/**
+     *
+     */
+	constructor() {
+		this.elements = [] // { target: element, attributes { class: value }
+	}
+
+}
