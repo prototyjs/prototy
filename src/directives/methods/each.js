a33e8f50bf2a47c32bab3b1c333d9eea23354660
Fedor Mironov <mironovline@gmail.com>
2026-03-28 18:06:00 +0500

    debug: improved the each-directive cause added moving an element to the right position

--- src/directives/methods/each.js
+++ src/directives/methods/each.js
@@ -7,25 +7,40 @@ export function each(container, array, setup) {
 	if (!container) {
 		return
 	}
+	//console.log(container)
 
 	// @ts-ignore
 	const nodeMap = container._nodeMap || (container._nodeMap = new WeakMap())
+	console.log(nodeMap)
 	const children = container.children
+	console.log(children)
 	const arrLength = array?.length || 0
+	console.log(arrLength)
 
 	if (!arrLength) {
 		container.innerHTML = ''
 		return
 	}
 
+	const existingNodes = new Map()
+	for (let i = 0; i < children.length; i++) {
+		const child = children[i]
+		if (child._boundItem) {
+			existingNodes.set(child._boundItem, child)
+		}
+	}
+	console.log(existingNodes)
+
 	for (let i = 0; i < arrLength; i++) {
 		const item = array[i]
 		let node = nodeMap.get(item)
 
 		if (!node) {
-
+			node = existingNodes.get(item)
+		}
+		if (!node) {
 			node = container._template.cloneNode(true)
-
+			node._boundItem = item
 			nodeMap.set(item, node)
 			setup(node, item)
 		}
